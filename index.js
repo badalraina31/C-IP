4fb48c9915a2cc482a0004c131e61397dcb47a94
Liam <creatiionhxx@gmail.com>
2018-11-10 11:05:34 +0000

    replaced copy-paste with clipboardy

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const electron = require('electron');
-const { app, Tray, Menu, BrowserWindow } = electron;
+const {
+    app,
+    Tray,
+    Menu,
+    BrowserWindow
+} = electron;
 const path = require('path');
 const iconPath = path.join(__dirname, 'iconTemplate@3x.png');
 let appIcon = null;
@@ -8,31 +13,35 @@ let win = null;
 const ip = require('ip');
 const publicIp = require('public-ip');
 var ExternalIP;
-var ncp = require("copy-paste");
+var ncp = require('clipboardy');
 
 app.on('ready', () => {
     publicIp.v4().then(eip => {
         ExternalIP = eip;
-        win = new BrowserWindow({show: false});
+        win = new BrowserWindow({
+            show: false
+        });
         appIcon = new Tray(iconPath);
-        var contextMenu = Menu.buildFromTemplate([
-            {
-            label: "Copy WLAN: " + ip.address("public"),
-            click: function() {
-                ncp.copy(ip.address("public"));
+        var contextMenu = Menu.buildFromTemplate([{
+                label: "Copy WLAN: " + ip.address("public"),
+                click: function () {
+                    ncp.writeSync(ip.address("public"));
                 }
             },
             {
-            label: "Copy Public: " + ExternalIP,
-            click: function() {
-                ncp.copy(ExternalIP);
+                label: "Copy Public: " + ExternalIP,
+                click: function () {
+                    ncp.writeSync(ExternalIP);
                 }
             },
-            {type: 'separator'},
-            { label: 'Quit',
-            accelerator: 'CommandOrControl+Q',
-            selector: 'terminate:',
-            click: function() {
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'CommandOrControl+Q',
+                selector: 'terminate:',
+                click: function () {
                     app.quit();
                     app.exit();
                 }
